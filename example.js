b364d842f8fc75ef59a1bdc14f17c2d3ed751bed
Sefinek Actions <contact@sefinek.net>
2024-03-21 20:40:47 +0100

    Update example.js

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ const emailValidator = require('./src/index.js');
 const email = 'contact@sefinek.net';
 
 if (emailValidator.test(email)) {
-	console.log(`Email ${email} is valid!`);
+	console.log(`Email ${email} is valid.`);
 } else {
 	console.log(`Email ${email} is NOT valid!`);
 }
